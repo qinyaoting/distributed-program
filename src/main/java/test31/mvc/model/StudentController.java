7176a2dc87e975c98f6cdd52ef1503c7e5501aa1
qyt <qinyaoting@gmail.com>
2017-09-06 10:23:30 +0800

    remove idea setting files

---
+++ src/main/java/test31/mvc/model/StudentController.java
@@ -0,0 +1,12 @@
+package test31.mvc.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chin
+ * Date: 9/5/17
+ * Time: 2:12 PM
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class StudentController {
+}
