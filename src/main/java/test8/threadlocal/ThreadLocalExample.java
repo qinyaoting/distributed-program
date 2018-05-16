fc016fef465b9ca9632534662b20cc27c5c4bbc1
qyt <qinyaoting@gmail.com>
2018-05-16 18:10:32 +0800

    object mapper

---
+++ src/main/java/test8/threadlocal/ThreadLocalExample.java
@@ -0,0 +1,48 @@
+package test8.threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Random;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chin
+ * Date: 5/16/18
+ * Time: 4:13 PM
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class ThreadLocalExample implements Runnable{
+
+    // SimpleDateFormat is not thread-safe, so give one to each thread
+    private static final ThreadLocal<SimpleDateFormat> formatter = new ThreadLocal<SimpleDateFormat>(){
+        @Override
+        protected SimpleDateFormat initialValue()
+        {
+            return new SimpleDateFormat("yyyyMMdd HHmm");
+        }
+    };
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadLocalExample obj = new ThreadLocalExample();
+        for(int i=0 ; i<10; i++){
+            Thread t = new Thread(obj, ""+i);
+            //Thread.sleep(new Random().nextInt(1000));
+            t.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread Name= "+Thread.currentThread().getName()+" default Formatter = "+formatter.get().toPattern());
+        try {
+            Thread.sleep(new Random().nextInt(1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //formatter pattern is changed here by thread, but it won't reflect to other threads
+        formatter.set(new SimpleDateFormat());
+
+        System.out.println("Thread Name= "+Thread.currentThread().getName()+" formatter = "+formatter.get().toPattern());
+    }
+
+}
